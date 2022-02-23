f8936d6983a53fdadfcc9aff10f7b99d0c26fdad
Gianluca Benassi <benox1199@gmail.com>
2022-02-23 15:49:13 +0100

    vue header style

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,6 +1,24 @@
 <template>
     <header>
-        <h2>header</h2>
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+
+                <a class="navbar-brand" href="/">MyBlog</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/admin/home">Dashboard</a>
+                        </li>
+                    </ul>
+
+                </div>
+
+            </div>
+        </nav>
     </header>
 </template>
 
--- resources/js/front.js
+++ resources/js/front.js
@@ -4,9 +4,7 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-window.axios = require('axios');
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+require('./bootstrap');
 
 window.Vue = require('vue');
 
